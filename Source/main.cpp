d13a67cfc13c48997d28b79cb366b0218f1a6245
Your Name <you@example.com>
2026-08-15 16:44:54 +0800

    chore: remove Build directory from tracking, fix main.cpp Vec template

--- Source/main.cpp
+++ Source/main.cpp
@@ -7,9 +7,5 @@ int main(){
     engine.Init();
     engine.Run();
 
-    RandEngine::Core::Math::Vec<float, 3> vec = {1.0f, 2.0f, 3.0f};
-
-    std::cout << vec * RandEngine::Core::Math::Mat<float, 3,3>::MakeIdentity()  << std::endl;
-
     return 0;
 }
