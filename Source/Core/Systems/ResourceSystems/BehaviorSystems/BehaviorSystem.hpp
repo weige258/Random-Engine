5cd52a5614b17097435166edfadf5d3a7a5e95f9
Your Name <you@example.com>
2026-08-18 17:57:21 +0800

    1

--- Source/Core/Systems/ResourceSystems/BehaviorSystems/BehaviorSystem.hpp
+++ Source/Core/Systems/ResourceSystems/BehaviorSystems/BehaviorSystem.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "Core/Behaviors/BaseBehavior/BaseBehavior.hpp"
 #include <memory>
 #include<vector>
 namespace RandEngine::Core::Systems::BehaviorSystems{
 
 class BehaviorSystem {
     private:
-    std::vector<int> all_behaviors;
+    std::vector<std::shared_ptr<Behaviors::BaseBehavior>> all_behaviors;
 
     public:
 };
